dae8b94eb5a069ebfd51c36926ed3494bb06712f
AdonayCalix <adonaymorazan@gmail.com>
2017-04-12 22:50:29 -0600

    Archivo Principal

--- ProyectoFinal/main.cpp
+++ ProyectoFinal/main.cpp
@@ -1,12 +1,9 @@
-#include <iostream>
-#include <iomanip>
 #include "Cabecera.h"
-
-using namespace std;
 
 int main()
 {
-    mensaje MensajeTitulo;
+    Mensaje MensajeTitulo;
     MensajeTitulo.tituloCabecera();
+    MensajeTitulo.menuPrincipal();
     return 0;
 }
