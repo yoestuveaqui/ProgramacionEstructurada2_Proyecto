92ebd12d5365701b1cfbfc81bf2746d8b3d0d7e8
AdonayCalix <adonaymorazan@gmail.com>
2017-04-12 22:08:05 -0600

    Texto que ira siempre en la parte superior de la consola

---
+++ ProyectoFinal/Cabecera.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+class mensaje{
+    public:
+        void tituloCabecera(){
+            cout<<setw(20)<<"----------------------------------------------------------------"<<endl;
+            cout<<setw(20)<<"                    TIENDA DEPARTAMENTAL"<<endl;
+            cout<<setw(20)<<"----------------------------------------------------------------"<<endl;
+            cout<<setw(20)<<"   CAMISAS| PANTALONES/FALDAS| ZAPATOS| JOYERIA| JUGUETERIA"<<endl;
+            cout<<setw(20)<<"      DERECHOS RESERVADOS A TEAM JOW 2017| PROYECTO FINAL "<<endl;
+            cout<<setw(20)<<"----------------------------------------------------------------"<<endl;
+        }
+};
+
+
