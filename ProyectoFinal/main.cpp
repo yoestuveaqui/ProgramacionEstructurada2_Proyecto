d7fbdde4f0bca35e77d034181806336cebe8fe75
AdonayCalix <adonaymorazan@gmail.com>
2017-04-13 11:16:21 -0600

    Archivo principal
    Se ha empezado a trabajar en la estrcutura principal del
    programa, todavia no es util pero lo importante es tener el esqueleto
    funcionando

--- ProyectoFinal/main.cpp
+++ ProyectoFinal/main.cpp
@@ -1,9 +1,35 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
 #include "Cabecera.h"
 
-int main()
-{
-    Mensaje MensajeTitulo;
-    MensajeTitulo.tituloCabecera();
-    MensajeTitulo.menuPrincipal();
+int main(){
+    string bandera="S";
+    int opcionCliente;
+    Mensaje titulo;
+    Mensaje Menu;
+    titulo.tituloCabecera();
+    while(bandera=="S"||bandera=="s"){
+        system("cls");
+        switch(Menu.menuPrincipal()){
+        case 1:
+            break;
+        case 2:
+            opcionCliente=Menu.menuCliente();
+            switch(opcionCliente){
+            default:
+                cout<<"Opcion ingesada invalida"<<endl;
+                break;
+            }
+            break;
+        default:
+            cout<<"Opcion ingresada invalida"<<endl;
+            break;
+        }
+        cout<<"Desea volver al menur principal? S/N"<<endl;
+        cout<<"Ingrese una opcion [ ]\b\b";
+        cin>>bandera;
+
+    }
     return 0;
 }
