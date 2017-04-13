9a324c3abe19514085c19064f185c8f862cb8db9
AdonayCalix <adonaymorazan@gmail.com>
2017-04-12 22:50:19 -0600

    Se agrego unos menus
    Se creo la clase de Mensaje la cual contedra todos los menus y mensajes
    que deba mostrar el sistema

--- ProyectoFinal/Cabecera.h
+++ ProyectoFinal/Cabecera.h
@@ -1,18 +1,49 @@
 #include<iostream>
-#include<iomanip>
 
 using namespace std;
 
-class mensaje{
+class Mensaje{
     public:
         void tituloCabecera(){
-            cout<<setw(20)<<"----------------------------------------------------------------"<<endl;
-            cout<<setw(20)<<"                    TIENDA DEPARTAMENTAL"<<endl;
-            cout<<setw(20)<<"----------------------------------------------------------------"<<endl;
-            cout<<setw(20)<<"   CAMISAS| PANTALONES/FALDAS| ZAPATOS| JOYERIA| JUGUETERIA"<<endl;
-            cout<<setw(20)<<"      DERECHOS RESERVADOS A TEAM JOW 2017| PROYECTO FINAL "<<endl;
-            cout<<setw(20)<<"----------------------------------------------------------------"<<endl;
+            cout<<"\t\t\t----------------------------------------------------------------"<<endl;
+            cout<<"\t\t\t                      TIENDA DEPARTAMENTAL"<<endl;
+            cout<<"\t\t\t----------------------------------------------------------------"<<endl;
+            cout<<"\t\t\t   CAMISAS| PANTALONES/FALDAS| ZAPATOS| JOYERIA| JUGUETERIA"<<endl;
+            cout<<"\t\t\t      DERECHOS RESERVADOS A TEAM JOW 2017| PROYECTO FINAL "<<endl;
+            cout<<"\t\t\t----------------------------------------------------------------"<<endl;
+        }
+        void menuPrincipal(){
+            cout<<"\t\t\t ------------------------"<<endl;
+            cout<<"\t\t\t 1- Ingresar como administrador"<<endl;
+            cout<<"\t\t\t 2- Ingresar como cliente"<<endl;
+            cout<<"\t\t\t 3- Salir del sistema"<<endl;
+            cout<<"\t\t\t -------------------------"<<endl;
+            cout<<"\t\t\t Ingrese su opcion [ ]"<<"\b\b";
+        }
+        void menuAdministrador(){
+            cout<<"\t\t\t    Acceso Administrativo | Opciones del sistema"<<endl;
+            cout<<"\t\t\t****************************************************"<<endl;
+            cout<<"\t\t\t1- Ingresar Articulo"<<endl;
+            cout<<"\t\t\t2- Modificar Articulo"<<endl;
+            cout<<"\t\t\t3- Eliminar Articulo"<<endl;
+            cout<<"\t\t\t4- Buscar Articilo"<<endl;
+            cout<<"\t\t\t5- Ver inventario de Articulos"<<endl;
+            cout<<"\t\t\t6- Salir"<<endl;
+            cout<<"\t\t\t*****************************************************"<<endl;
+            }
+
+        void menuCliente(){
+            cout<<"\t\t\t----------------------------------------"<<endl;
+            cout<<"\t\t\t       Bienvenidos a nuestra tienda"<<endl;
+            cout<<"\t\t\t----------------------------------------"<<endl;
+            cout<<"\t\t\t Escoga una categoria"
         }
 };
+
+
+
+
+
+
 
 
