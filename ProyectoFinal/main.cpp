b1223413cfab439dd758f3ae3dc720b336856961
AdonayCalix <adonaymorazan@gmail.com>
2017-04-13 18:09:24 -0600

    Archivo Principal

--- ProyectoFinal/main.cpp
+++ ProyectoFinal/main.cpp
@@ -5,31 +5,36 @@
 
 int main(){
     string bandera="S";
-    int opcionCliente;
-    Mensaje titulo;
-    Mensaje Menu;
+    string opcionCliente;
+    Mensaje titulo, Menu;
+    Seguridad Sesion;
     titulo.tituloCabecera();
     while(bandera=="S"||bandera=="s"){
         system("cls");
         switch(Menu.menuPrincipal()){
         case 1:
-            break;
-        case 2:
-            opcionCliente=Menu.menuCliente();
-            switch(opcionCliente){
-            default:
-                cout<<"Opcion ingesada invalida"<<endl;
-                break;
+            if(Sesion.menuLogeo()==false){
+                cout<<"Alto ahi malechor"<<endl;
+            }
+            else{
+                cout<<"Todo tuani"<<endl;
             }
             break;
-        default:
-            cout<<"Opcion ingresada invalida"<<endl;
+        case 2:
+            cout<<endl;
+            Menu.menuDepartamentos();
             break;
-        }
-        cout<<"Desea volver al menur principal? S/N"<<endl;
-        cout<<"Ingrese una opcion [ ]\b\b";
-        cin>>bandera;
 
+        case 3:
+            exit(0);
+
+        default:
+            cout<<"Opcion Invalida"<<endl;
+
+        }
+        cout<<"\t\t\tDesea volver al menur principal? S/N"<<endl;
+        cout<<"\t\t\tIngrese una opcion [ ]\b\b";
+        cin>>bandera;
     }
     return 0;
 }
